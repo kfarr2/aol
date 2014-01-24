69e53d13edccdf1a5a232c03c16ea0c69a7c971f
Matt Johnson <mdj2@pdx.edu>
2014-01-24 19:11:33 +0000

    Facility test case

--- aol/lakes/tests/test_models.py
+++ aol/lakes/tests/test_models.py
@@ -2,7 +2,7 @@ import os
 from django.test import TestCase
 from django.contrib.gis.geos import GEOSGeometry
 from django.conf import settings as SETTINGS
-from ..models import NHDLake as Lake, FishingZone, County, HUC6, LakeCounty, Photo
+from ..models import NHDLake as Lake, FishingZone, County, HUC6, LakeCounty, Photo, Facility
 
 class LakeManagerTest(TestCase):
     fixtures = ['lakes.json']
@@ -127,3 +127,39 @@ class PhotoTest(TestCase):
             os.remove(thumb_path)
         except OSError:
             pass
+
+class FacilityManagerTest(TestCase):
+    fixtures = ['lakes.json']
+
+    def setUp(self):
+        super(FacilityManagerTest, self).setUp()
+        self.f = Facility(
+            name="foo",
+            waterbody="foo",
+            islake=0,
+            type=1,
+            telephone="555",
+            ramp_type="asdf",
+            moorage="asdf",
+            trailer_park="asdf",
+            transient="asdf",
+            launch_fee="asdf",
+            restroom="asdf",
+            supplies="asdf",
+            gas_on_water="asdf",
+            diesel_on_water="asdf",
+            fish_cleaning="asdf",
+            pumpout="asd",
+            dump_station="asdf",
+            managed_by="asdf",
+            latitude=10.0,
+            longitude=10.0,
+            boater_ser="asdf",
+            icon_url="asdf",
+            the_geom=GEOSGeometry("Point(30 20)"),
+            lake=Lake.objects.get(reachcode=123)
+        )
+        self.f.save()
+
+    def test_to_kml(self):
+        self.assertTrue(Facility.objects.to_kml(bbox=(-50000, -50000, 50000, 50000))[0].kml)
